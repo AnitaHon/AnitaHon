5fecb34a7f31549515d7247768538a1b73968e32
AnitaHon <Ahon2@myseneca.ca>
2024-11-19 18:40:42 -0500

    e

--- main.js
+++ main.js
@@ -8,6 +8,16 @@ new Glide('.glide').mount();
 document.addEventListener('DOMContentLoaded', () => {
     new Glide('.glide').mount();
 });
+const config = {
+    type: 'carousel',  // Make sure it's a carousel
+    perView: 1,        // Show only 1 slide at a time
+    focusAt: 'center', // Focus the current slide at the center of the view
+    gap: 0,            // No gap between slides
+    autoplay: false,   // Disable autoplay if not needed
+}
 
+document.addEventListener('DOMContentLoaded', () => {
+    new Glide('.glide', config).mount();
+});
 /*----------------------------------------------  -----------------------------------------------*/
 
